575241d48e255d878bc81dff6a4e49efbea3fc28
Steve Lacy <me@slacy.me>
2016-11-16 11:37:17 -0700

    Add mapbox style + immutable updates

--- src/views/Map/index.js
+++ src/views/Map/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Component } from 'core'
+import update from 'immutability-helper'
 import MapGl from 'react-map-gl'
 import './index.sass'
 
 const accessToken = 'pk.eyJ1Ijoic3RldmVsYWN5IiwiYSI6ImNpdmtpejc4NTA4NmUyb2x2YTB5cnlremgifQ.KsPYeRPvv5AJKF3g84LOHg'
+const mapStyle = 'mapbox://styles/stevelacy/civkk44n1007t2kqqewcqsa4k'
+
 
 export default class MapView extends Component {
   constructor (props) {
@@ -21,20 +24,29 @@ export default class MapView extends Component {
   static displayName = 'MapView'
 
   handleViewportChange (viewport = {}) {
-    this.setState({ viewport })
+    this.setState({
+      viewport: update(this.state.viewport, { $merge: viewport })
+    })
+  }
+
+  handleMapLoad (e) {
+    if (!e) return
+    this.setState({
+      viewport: update(this.state.viewport, {$merge: { width: e.offsetWidth }})
+    })
   }
 
   render() {
     return (
       <div
-        ref={(e) => e ? this.setState({ width: e.offsetWidth }) : null}
+        ref={this.handleMapLoad}
         className='map-view'>
         <MapGl
-          width={this.state.width}
           height={400}
-          {...this.state.viewport}
+          mapStyle={mapStyle}
           mapboxApiAccessToken={accessToken}
-          onChangeViewport={this.handleViewportChange} />
+          onChangeViewport={this.handleViewportChange}
+          {...this.state.viewport} />
       </div>
     )
   }
