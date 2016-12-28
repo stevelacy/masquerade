26ac31c2770b5f2edbcb65e0e0ac4e7c13a47748
Steve Lacy <me@slacy.me>
2016-12-27 19:46:38 -0700

    add source view

---
+++ src/views/Sources/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Component } from 'core'
+import './index.sass'
+
+export default class SourcesView extends Component {
+  static displayName = 'SourcesView'
+  static propTypes = {
+  }
+
+  render () {
+    return (
+      <div className='sources-view'>
+        sources
+      </div>
+    )
+  }
+}
